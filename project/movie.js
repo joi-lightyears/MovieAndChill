554d7ef39d37dad104825c0580bb5cef7a02e532
joi-lightyears <kunthanhdat@gmail.com>
2023-01-14 12:48:46 +0700

    add ep function

--- project/movie.js
+++ project/movie.js
@@ -1,6 +1,6 @@
-const slug = localStorage.getItem("slug")
+let slug = localStorage.getItem("slug")
 let apiFilm = `https://ophim1.com/phim/${slug}`
-
+let movie = null
 
 const bannerImg = document.querySelector('.banner-img-tag')
 const bannerTitle = document.querySelector('.banner-title')
@@ -19,7 +19,12 @@ function handleClose(){
     movieModal.classList.toggle('open')
     iframe.setAttribute('src', '')
 }
+const episodes = document.querySelector('.episodes')
+episodes.addEventListener('click', function(event){
+    event.stopPropagation()
+})
 
+// get data from api
 const getData= async(url)=>{
     const response = await fetch(url)
     if(response.status===200){
@@ -28,8 +33,10 @@ const getData= async(url)=>{
         return movieData
     }
 }
+
 async function init(){
-    const movie = await getData(apiFilm)
+    const movieData = await getData(apiFilm)
+    movie = movieData
     const film = movie.movie
     let img = film.poster_url
     if(!film.poster_url){
@@ -57,8 +64,46 @@ const watchBtn = document.querySelector('.watch-btn')
 watchBtn.addEventListener('click', handleWatch)
 async function handleWatch(){
     movieModal.classList.toggle('open')
-    const movie = await getData(apiFilm)
+    renderEpisodes(movie)
     const ep = movie.episodes[0].server_data[0].link_embed
-    console.log(ep)
     iframe.setAttribute('src', ep)
 }
+
+
+const epContainer = document.querySelector('.episodes-container')
+function renderEpisodes(movie){
+    epContainer.innerHTML = ''
+    const epList = movie.episodes[0].server_data
+    epList.forEach((ep)=>{
+        const epTag = document.createElement('div')
+        epTag.className = `ep ${ep.slug}`
+        epTag.textContent = ep.name
+        epTag.addEventListener('click',()=>{
+            iframe.setAttribute('src', ep.link_embed)
+        })
+        epContainer.appendChild(epTag)
+    })
+}
+
+// search ep
+const searchInput =  document.querySelector('.search-input')
+searchInput.addEventListener('keypress', async(e)=>{
+    if(e.key === 'Enter'){
+        epContainer.innerHTML = ''
+        const epsData = movie.episodes[0].server_data
+        epsData.forEach((ep)=>{
+            if(ep.slug.includes(e.target.value)){
+                const epTag = document.createElement('div')
+                epTag.className = `ep ${ep.slug}`
+                epTag.textContent = ep.name
+                epTag.addEventListener('click',()=>{
+                    iframe.setAttribute('src', ep.link_embed)
+                })
+                epContainer.appendChild(epTag)
+            }
+        })
+    }
+    if(!e.target.value){
+        renderEpisodes(movie)
+    }
+})
